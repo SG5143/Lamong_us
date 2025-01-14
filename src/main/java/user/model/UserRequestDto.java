6692375f7648e927bc5b479e628b44c03e4b2b96
sungwoni9 <hurdang99@naver.com>
2025-01-14 18:01:01 +0900

    Create new UserRequestDto file for user-felated response data

---
+++ src/main/java/user/model/UserRequestDto.java
@@ -0,0 +1,125 @@
+package user.model;
+
+public class UserRequestDto {
+	private String uuid;
+	private String username;
+	private String password;
+	private String nickname;
+	private String phone;
+	private String email;
+	private Boolean deleteStatus;
+	private String loginType;
+	private Boolean banStatus;
+	private String profileInfo;
+	private byte[] profileImage;
+	private int score;
+	private String apiKey;
+
+	public UserRequestDto() {
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Boolean getDeleteStatus() {
+		return deleteStatus;
+	}
+
+	public void setDeleteStatus(Boolean deleteStatus) {
+		this.deleteStatus = deleteStatus;
+	}
+
+	public String getLoginType() {
+		return loginType;
+	}
+
+	public void setLoginType(String loginType) {
+		this.loginType = loginType;
+	}
+
+	public Boolean getBanStatus() {
+		return banStatus;
+	}
+
+	public void setBanStatus(Boolean banStatus) {
+		this.banStatus = banStatus;
+	}
+
+	public String getProfileInfo() {
+		return profileInfo;
+	}
+
+	public void setProfileInfo(String profileInfo) {
+		this.profileInfo = profileInfo;
+	}
+
+	public byte[] getProfileImage() {
+		return profileImage;
+	}
+
+	public void setProfileImage(byte[] profileImage) {
+		this.profileImage = profileImage;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public String getApiKey() {
+		return apiKey;
+	}
+
+	public void setApiKey(String apiKey) {
+		this.apiKey = apiKey;
+	}
+
+}
