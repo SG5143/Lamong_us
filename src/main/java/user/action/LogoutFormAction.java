e0dc1e77822eaa5f42442b4159a5b9c879697ade
sungwoni9 <hurdang99@naver.com>
2025-01-15 15:16:59 +0900

    Implemented logout functionality by removing the user attribute from the session and invalidating the session 2025-01-15
    1. HttpSession: 세션에서 user 속성을 제거
    
    2. session.invalidate: 세션을 무효화하여 로그아웃 기능을 구현
    
    3. sendResponseStatusAndMessage: 성공 메시지와 상태 코드를 포함한 JSON 응답을 반환

--- src/main/java/user/action/LogoutFormAction.java
+++ src/main/java/user/action/LogoutFormAction.java
@@ -1,5 +1,40 @@
 package user.action;
 
-public class LogoutFormAction {
+import java.io.*;
+
+import org.json.*;
+
+import controller.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class LogoutFormAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+
+		session.removeAttribute("user");
+
+		session.invalidate();
+
+		sendResponseStatusAndMessage(response, HttpServletResponse.SC_OK, "로그아웃 성공");
+
+	}
+
+	private void sendResponseStatusAndMessage(HttpServletResponse response, int statusCode, String message)
+			throws IOException {
+		JSONObject jsonResponse = new JSONObject();
+		jsonResponse.put("status", statusCode);
+		jsonResponse.put("message", message);
+
+		String json = jsonResponse.toString();
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(json);
+
+	}
+	
+	
 
 }
