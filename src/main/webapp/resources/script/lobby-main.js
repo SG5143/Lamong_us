aefb57ed6d5b4fa72dae12add31f020bf666196a
ohserin <dhtpfls321@gmail.com>
2025-01-20 18:17:51 +0900

    Update lobby-main.js for room management and pagination 2025.01.20
    1. 방 생성 버튼 클릭 시 방을 생성하는 기능 추가
    2. 페이지 네이션 기능

--- src/main/webapp/resources/script/lobby-main.js
+++ src/main/webapp/resources/script/lobby-main.js
@@ -1,6 +1,16 @@
+import { createRoom, initializeRoomCreation } from './create-room.js';
+
+let currentPage = 1;
+let pageableCount;
 
 window.onload = () => {
 	fetchRoomList(1);
+	document.getElementById("submit-button").addEventListener("click", async (e) => {
+		e.preventDefault();
+		await createRoom(currentPage, fetchRoomList);
+	});
+
+	initializeRoomCreation();
 };
 
 const fetchRoomList = async (page = 1) => {
@@ -9,6 +19,8 @@ const fetchRoomList = async (page = 1) => {
 		if (!response.ok) throw new Error("Failed to fetch room list");
 
 		const data = await response.json();
+		pageableCount = data.Meta.pageable_count;
+
 		updateRoomList(data.GameRoom);
 	} catch (error) {
 		console.error("Error fetching room list:", error);
@@ -17,15 +29,31 @@ const fetchRoomList = async (page = 1) => {
 };
 
 const updateRoomList = (rooms) => {
+	console.log(rooms);
 	const gameRoomContainer = document.querySelector(".game-room");
 
 	gameRoomContainer.innerHTML = "";
 
-		console.log(rooms);
 	rooms.forEach((room) => {
-		
 		const roomElement = document.createElement("div");
 		roomElement.classList.add("room");
+
+		let borderColor = "";
+		let stateText = "";
+		const full = room.current_players == room.max_players;
+
+		if (room.state === "ready") {
+			borderColor = "#3ADE3C";
+			stateText = "대기중";
+		} else if (room.state === "playing") {
+			borderColor = "#484F5E";
+			stateText = "게임중";
+		} else if ((room.state === "ready" && full) || room.state === "full") {
+			borderColor = "#DE3A86";
+			stateText = "인원초과";
+		}
+
+		roomElement.style.border = `2px solid ${borderColor}`;
 
 		roomElement.innerHTML = `
             <div>
@@ -34,16 +62,34 @@ const updateRoomList = (rooms) => {
             </div>
             <div>
                 <div>
-                    <div>게임 진행 여부: ${room.max_players} </div>
-                    <div>${room.is_private ? "비공개" : "공개"}</div>
+                    <div>방 상태: ${stateText} </div>
+                    <div>${room.is_private ? "비번방" : "공개"}</div>
                 </div>
                 <div>
-                    <div>인원수: ${room.max_players}</div>
+                    <div>인원수: ${room.current_players} / ${room.max_players}</div>
                     <div>icon2</div>
                 </div>
             </div>
         `;
-
 		gameRoomContainer.appendChild(roomElement);
 	});
 };
+
+// 페이지 네이션
+const changePage = (direction) => {
+	if (direction === -1 && currentPage <= 1) {
+		alert("첫 페이지입니다.");
+		return;
+	}
+
+	if (direction === 1 && currentPage >= pageableCount) {
+		alert("마지막 페이지입니다.");
+		return;
+	}
+
+	currentPage += direction;
+	fetchRoomList(currentPage);
+};
+
+document.getElementById("prevPageBtn").addEventListener("click", () => changePage(-1));
+document.getElementById("nextPageBtn").addEventListener("click", () => changePage(1));
