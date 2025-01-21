b11f16ef0c01cbeeaa6c673085ecd43f8c1608b3
sungwoni9 <hurdang99@naver.com>
2025-01-21 18:47:01 +0900

    Add additional validation functions for nickname, email, and phone in validation.js
    2025.01.21
    1. 새로 추가된 validateName, validateEmail, validatePhone 함수로 각 필드의 유효성 검사
    2. 사용자 이름, 닉네임, 전화번호, 이메일 중복 체크를 위한 checkDuplUsername, checkDuplnickname, checkDuplPhone, checkDuplEmail 비동기 함수 추가
    3. 각 중복 체크 함수는 POST 요청을 통해 서버에서 유효성 확인 후 결과 반환

--- src/main/webapp/resources/script/validation/validation.js
+++ src/main/webapp/resources/script/validation/validation.js
@@ -6,14 +6,90 @@ export function updateErrorElementStyle(element, visible) {
 	}
 }
 
-// 아이디 유효성 검사
 export const validateUsername = (username) => {
 	const usernamePattern = /^[a-zA-Z0-9-_!@#$%^&*\.]{5,20}$/;
 	return usernamePattern.test(username);
 };
 
-// 비밀번호 유효성 검사
 export const validatePassword = (password) => {
 	const passwordPattern = /^(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,20}$/;
 	return passwordPattern.test(password);
 };
+
+export const validateName = (nickname) => {
+	const nicknamePattern = /^[a-zA-Z0-9가-힣]{2,12}$/;
+	return nicknamePattern.test(nickname);
+};
+
+export const validateEmail = (email) => {
+	const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+	return emailPattern.test(email);
+};
+
+export const validatePhone = (phone) => {
+	const phonePattern = /^010-\d{4}-\d{4}$/;
+	return phonePattern.test(phone);
+};
+
+export async function checkDuplUsername(username) {
+	const response = await fetch("/v1/members?command=search-username", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			"username": username
+		})
+	});
+	const json = await response.json();
+
+	return json.isValid;
+}
+
+export async function checkDuplnickname(nickname) {
+	const response = await fetch("/v1/members?command=search-nickname", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			"nickname": nickname
+		})
+	});
+	const json = await response.json();
+
+	return json.isValid;
+}
+
+export async function checkDuplPhone(phone) {
+	const response = await fetch("/v1/members?command=search-phone", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			"phone": phone
+		})
+	});
+	const json = await response.json();
+
+	return json.isValid;
+}
+
+export async function checkDuplEmail(email) {
+	const response = await fetch("/v1/members?command=search-email", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			"email": email
+		})
+	});
+	const json = await response.json();
+
+	return json.isValid;
+}
+
+
+
