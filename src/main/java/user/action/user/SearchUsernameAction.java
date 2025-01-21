f4655b32b670f75a790ab97f8bb6ed4e8da1846d
sungwoni9 <hurdang99@naver.com>
2025-01-21 18:51:43 +0900

    Add actions for checking duplicate phone, email, nickname, and username
    2025.01.21
    1. 전화번호 중복 검사를 위한 액션 추가
    2. 이메일 중복 검사를 위한 액션 추가
    3. 닉네임 중복 검사를 위한 액션 추가
    4. 아이디 중복 검사를 위한 액션 추가
    5. 각 액션에서 요청 본문 검증 후 중복 여부 반환
    6. 응답에 상태 및 메시지 포함하여 클라이언트에 전달

---
+++ src/main/java/user/action/user/SearchUsernameAction.java
@@ -0,0 +1,56 @@
+package user.action.user;
+
+import java.io.*;
+import java.sql.*;
+
+import org.apache.catalina.*;
+import org.json.*;
+
+import controller.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import user.model.user.*;
+import user.model.user.User;
+
+public class SearchUsernameAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		StringBuilder builder = new StringBuilder();
+
+		BufferedReader reader = request.getReader();
+		while (reader.ready()) {
+			builder.append(reader.readLine());
+		}
+
+		JSONObject reqData = new JSONObject(builder.toString());
+		JSONObject resData = new JSONObject();
+
+		if (!reqData.has("username")) {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			resData.put("status", HttpServletResponse.SC_BAD_REQUEST);
+			resData.put("error", "BAD REQUEST");
+			resData.put("message", "잘못된 요청입니다. 필수 키 값이 누락되었습니다.");
+			resData.put("timestamp", new Timestamp(System.currentTimeMillis()));
+		} else {
+			String username = reqData.getString("username");
+
+			UserDao userDao = UserDao.getInstance();
+			User user = userDao.findUserByUsername(username);
+
+			boolean isValid = user == null;
+
+			resData.put("isValid", isValid);
+		}
+
+		// 응답 준비
+		response.setContentType("application/json");
+		response.setCharacterEncoding("utf-8");
+
+		PrintWriter out = response.getWriter();
+		out.append(resData.toString());
+		out.flush();
+
+	}
+
+}
