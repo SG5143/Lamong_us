5a054d4cfbf7605bc9b903301f17b3860d0075a7
SG5143 <sun5143a@gmail.com>
2025-01-24 10:32:06 +0900

    Add GameConstants for WebSocket message handling 2025.01.24 1. 웹소켓에서 사용되는 메시지및 키값을 상수로 분리

---
+++ src/main/java/websoket/GameConstants.java
@@ -0,0 +1,26 @@
+package websoket;
+
+public class GameConstants {
+	
+	// 웹소켓 상수 정의
+    public static final String TYPE_MESSAGE = "MESSAGE";
+    public static final String TYPE_SET_TURN = "SET_TURN";
+    public static final String TYPE_GAME_START = "GAME_START";
+    public static final String TYPE_CUR_TURN = "CUR_TURN";
+    public static final String TYPE_CLIENT_INFO = "CLIENT_INFO";
+    public static final String TYPE_ROUND_INFO = "ROUND_INFO";
+    public static final String TYPE_ROUND_END ="ROUND_END";
+    
+    public static final String MESSAGE = "message";
+
+    public static final String TURN = "turn";
+    public static final String ROUND = "round";
+    public static final String SESSION_ID = "sessionId";
+    
+    public static final String CURRENT_TURN = "currentTurn";
+    public static final String CLIENT_INFO = "clientInfo";
+    
+    public static final String TOPIC ="topic";
+    public static final String KEYWORD = "keyword";
+    
+}
