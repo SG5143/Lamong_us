eeff91de2795fa9a966fa5fb2dec72ef6a421859
SG5143 <sun5143a@gmail.com>
2025-01-27 00:35:57 +0900

    Add new WebSocket message types for vote and game end 2025.01.27 1. 게임의 투표와 결과, 게임 종료 상태에 관련된 메시지 타입 추가 2. SYSTEM_MESSAGE, STATE_CHANGE, VOTE, VOTE_RESULT, GAME_END 등의 상수 추가

--- src/main/java/websoket/GameConstants.java
+++ src/main/java/websoket/GameConstants.java
@@ -4,12 +4,19 @@ public class GameConstants {
 	
 	// 웹소켓 상수 정의
     public static final String TYPE_MESSAGE = "MESSAGE";
+    
+    public static final String TYPE_SYSTEM_MESSAGE = "SYSTEM_MESSAGE";
+    public static final String TYPE_STATE_CHANGE = "STATE_CHANGE";
+    
     public static final String TYPE_SET_TURN = "SET_TURN";
     public static final String TYPE_GAME_START = "GAME_START";
     public static final String TYPE_CUR_TURN = "CUR_TURN";
     public static final String TYPE_CLIENT_INFO = "CLIENT_INFO";
     public static final String TYPE_ROUND_INFO = "ROUND_INFO";
-    public static final String TYPE_ROUND_END ="ROUND_END";
+    public static final String TYPE_VOTE = "VOTE";
+	public static final String TYPE_VOTE_RESULT = "VOTE_RESULT";
+    public static final String TYPE_GAME_END = "GAME_END";
+   
     
     public static final String MESSAGE = "message";
 
@@ -22,5 +29,13 @@ public class GameConstants {
     
     public static final String TOPIC ="topic";
     public static final String KEYWORD = "keyword";
+	public static final String VOTE = "vote";
+	public static final String VOTE_RESULT_MOST_VOTED= "voteResultMostVoted";
+	public static final String VOTE_RESULT_MAX_VOTES= "voteResultMaxVotes";
+	public static final Object GAME_END = "gameEnd";
+
+	public static final Object GAME_STATE = "gamsState";
+
+
     
 }
